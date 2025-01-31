8371428e9d5e76170a742d392d5211fa62a10480
Sahriz <94785672+Sahriz@users.noreply.github.com>
2025-01-31 20:50:21 +0100

    Fixed issues with padding
    Padding with std430 (ssbos) always padds inputs as multiples of 16, so we pad using an 8 byte character to fix the issue.

--- src/main.cpp
+++ src/main.cpp
@@ -15,13 +15,14 @@
 
 
 struct Primitive {
-    vec3 vertex1;
-    vec3 vertex2;
-    vec3 vertex3;
-    vec3 color;
-    vec3 normal;
-    int ID; // 0 == Triangle, 1 == Sphere
-    float bounceOdds; //Odds that the ray would bounce off of the surface.
+    alignas(16) vec3 vertex1;
+    alignas(16) vec3 vertex2;
+    alignas(16) vec3 vertex3;
+    alignas(16) vec3 color;
+    alignas(16) vec3 normal;
+    alignas(4) int ID; // 0 == Triangle, 1 == Sphere
+    alignas(4) float bounceOdds; //Odds that the ray would bounce off of the surface.
+    alignas(8) char padding;
 };
 
 
